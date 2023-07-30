317c97c5e2caec32256cfcfee9e0ce800fc8602c
Tyler <github.26ac3d4d@alias.tylerw.co.uk>
2023-07-30 21:37:20 +0100

    add version command

---
+++ cmd/version.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = &cobra.Command{
+	Use:           "version [flags]",
+	Short:         "Returns the version",
+	SilenceErrors: true,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Version: v1.1.1")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
