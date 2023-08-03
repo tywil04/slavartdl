13a05e8edde0dc1928b6e5894737dd71b44510fe
Tyler <github.26ac3d4d@alias.tylerw.co.uk>
2023-08-03 17:56:36 +0100

    allow for updating structure of config

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -12,6 +13,25 @@ import (
 var ConfigDir string
 var ConfigName string
 var ConfigFile string
+
+var DefaultStructure = map[string]any{
+	"divoltsessiontokens": []string{},
+	"downloadcmd": map[string]any{
+		"outputdir": "",
+		"quality":   0,
+		"timeout": map[string]any{
+			"seconds": 0,
+			"minutes": 2,
+		},
+		"ignore": map[string]any{
+			"cover":   false,
+			"subdirs": false,
+		},
+		"skip": map[string]any{
+			"unzip": false,
+		},
+	},
+}
 
 func Load(defaultHandling bool, customPath string) error {
 	// default handling means it uses the default config
@@ -61,27 +81,13 @@ func Load(defaultHandling bool, customPath string) error {
 				return fmt.Errorf("failed to create user config directory")
 			}
 
-			var defaultConfig = bytes.NewBuffer([]byte(`{
-	"divoltsessiontokens": [],
-	"downloadcmd": {
-		"outputdir": "",
-		"quality": 0,
-		"timeout": {
-			"seconds": 0,
-			"minutes": 2
-		},
-		"ignore": {
-			"cover": false,
-			"subdirs": false
-		},
-		"skip": {
-			"unzip": false
-		}
-	}
-}`))
+			defaultConfig, err := json.Marshal(DefaultStructure)
+			if err != nil {
+				return fmt.Errorf("failed to marshal default config")
+			}
 
 			// load default config
-			if err := viper.ReadConfig(defaultConfig); err != nil {
+			if err := viper.ReadConfig(bytes.NewBuffer(defaultConfig)); err != nil {
 				return fmt.Errorf("failed to load default config")
 			}
 
@@ -95,9 +101,19 @@ func Load(defaultHandling bool, customPath string) error {
 		}
 	}
 
+	UpdateStructure()
+
 	return nil
 }
 
 func Offload() error {
 	return viper.WriteConfigAs(ConfigFile)
+}
+
+func UpdateStructure() {
+	for key, value := range DefaultStructure {
+		if !viper.IsSet(key) {
+			viper.Set(key, value)
+		}
+	}
 }
