9973852bcd1dbeee3b848316555b0ce723415abf
Tyler <github.26ac3d4d@alias.tylerw.co.uk>
2023-08-18 20:35:05 +0100

    bump version to v1.1.11

--- internal/update/update.go
+++ internal/update/update.go
@@ -18,7 +18,7 @@ import (
 	"github.com/tywil04/slavartdl/internal/helpers"
 )
 
-const Version = "v1.1.10"
+const Version = "v1.1.11"
 
 const assetNameTemplate = "slavartdl-%s-%s-%s.%s"
 const signatureNameTemplate = "slavartdl-%s-%s-%s.%s.md5"
